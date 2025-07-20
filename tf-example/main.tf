d65a6cbc2c9a866375a8a14d246754112be5bca9
octavia2000 <75098685+octavia2000@users.noreply.github.com>
2025-07-20 16:46:42 +0000

    corrected

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -2,24 +2,20 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# Create a VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Create Subnet
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-2a"
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# Route Table
 resource "aws_route_table" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -29,13 +25,11 @@ resource "aws_route_table" "main" {
   }
 }
 
-# Associate Route Table with Subnet
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.main.id
   route_table_id = aws_route_table.main.id
 }
 
-# Security Group
 resource "aws_security_group" "main" {
   name        = "allow_ssh"
   description = "Allow SSH"
@@ -56,11 +50,10 @@ resource "aws_security_group" "main" {
   }
 }
 
-# EC2 Instance
 resource "aws_instance" "app_server" {
-  ami                         = "ami-053b0d53c279acc90"  # Ubuntu 22.04 LTS (us-east-2)
+  ami                         = "ami-053b0d53c279acc90"  # Ubuntu 22.04 LTS for us-east-2
   instance_type               = "t3.micro"
-  key_name                    = "app-ssh-key"            # Must match existing key
+  key_name                    = "app-ssh-key"  # This must already exist in AWS
   subnet_id                   = aws_subnet.main.id
   vpc_security_group_ids      = [aws_security_group.main.id]
 

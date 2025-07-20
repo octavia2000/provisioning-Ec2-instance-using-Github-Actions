30a9b70f3d6a385a9691e0b81fdde32879775232
octavia2000 <75098685+octavia2000@users.noreply.github.com>
2025-07-20 16:07:28 +0000

    vpc updates

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -18,53 +18,10 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2"
-}
-
-resource "aws_security_group" "main" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_instance" "app_server" {
-  ami                         = "ami-053b0d53c279acc90"
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.main.id
-  vpc_security_group_ids      = [aws_security_group.main.id]
-
-  tags = {
-    Name = "UbuntuAppServer"
-  }
-}
-
-
-  tags = {
-    Name = "AppServer"
-  }
-}
-
+  ami           = ami-000674c4ed29c3510
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
 
   tags = {
     Name = var.ec2_name

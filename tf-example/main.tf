136cdbb1495d7956e3c2268f3510e73d11059e2c
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2025-07-23 22:02:32 +0100

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -1,18 +1,24 @@
-provider "aws" {
-  region = "us-east-2"
+data "aws_subnet" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+
+  # You can also specify AZ if you want, like "us-east-2a"
+  availability_zone = "us-east-2a"
 }
 
-# Get the default VPC
-data "aws_vpc" "default" {
-  default = true
+variable "ec2_name" {
+  description = "Name tag for the EC2 instance"
+  type        = string
+  default     = "UbuntuAppServer"
 }
 
-# Get a default subnet (you can filter by AZ if you want)
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-# Security group allowing SSH
 resource "aws_security_group" "main" {
   name        = "allow_ssh_default_vpc"
   description = "Allow SSH"
@@ -33,15 +39,14 @@ resource "aws_security_group" "main" {
   }
 }
 
-# EC2 instance in default VPC
 resource "aws_instance" "app_server" {
-  ami                    = "ami-053b0d53c279acc90"  # Ubuntu 22.04 LTS in us-east-2
+  ami                    = "ami-053b0d53c279acc90"
   instance_type          = "t3.micro"
-  key_name               = "app-ssh-key"  # Ensure this exists in your AWS account
-  subnet_id              = data.aws_subnet_ids.default.ids[0]  # Use the first default subnet
+  key_name               = "app-ssh-key"
+  subnet_id              = data.aws_subnet.default.id
   vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
-    Name = "UbuntuAppServer"
+    Name = var.ec2_name
   }
 }

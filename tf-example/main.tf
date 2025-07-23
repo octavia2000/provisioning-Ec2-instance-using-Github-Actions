6d1024ae46ba74903d2e393b897dfe4a02d5d846
Michael Agbiaowei <101793657+michaelagbiaowei@users.noreply.github.com>
2025-07-23 21:50:03 +0100

    Update main.tf

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -2,38 +2,21 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
+# Get a default subnet (you can filter by AZ if you want)
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
-}
-
+# Security group allowing SSH
 resource "aws_security_group" "main" {
-  name        = "allow_ssh"
+  name        = "allow_ssh_default_vpc"
   description = "Allow SSH"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -50,12 +33,13 @@ resource "aws_security_group" "main" {
   }
 }
 
+# EC2 instance in default VPC
 resource "aws_instance" "app_server" {
-  ami                         = "ami-053b0d53c279acc90"  # Ubuntu 22.04 LTS for us-east-2
-  instance_type               = "t3.micro"
-  key_name                    = "app-ssh-key"  # This must already exist in AWS
-  subnet_id                   = aws_subnet.main.id
-  vpc_security_group_ids      = [aws_security_group.main.id]
+  ami                    = "ami-053b0d53c279acc90"  # Ubuntu 22.04 LTS in us-east-2
+  instance_type          = "t3.micro"
+  key_name               = "app-ssh-key"  # Ensure this exists in your AWS account
+  subnet_id              = data.aws_subnet_ids.default.ids[0]  # Use the first default subnet
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
     Name = "UbuntuAppServer"
